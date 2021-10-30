30a9355ae3d0841cd49a5354242ee033d83f3d22
shahidmonowarr <shahidmonowar21@gmail.com>
2021-10-30 15:22:00 +0600

    package connect

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express()
 const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3bc4k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
@@ -14,7 +21,36 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        console.log('database connected');
+        console.log('database connected now');
+        const database = client.db('travelBooking');
+        const packagesCollection = database.collection('packages');
+
+        //Get APi
+        app.get('/packages', async (req, res) => {
+            const cursor = packagesCollection.find({});
+            const packages = await cursor.toArray();
+            res.send(packages);
+        });
+
+        //get singlePackage
+        app.get('/packages/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log('getting specific package', id);
+            const query = { _id: ObjectId(id) };
+            const singlePackage = await packagesCollection.findOne(query);
+            res.json(singlePackage);
+        })
+
+
+        //post API
+        app.post('/packages', async (req, res) => {
+            const package = req.body;
+            console.log('hit the post api', package);
+
+            const result = await packagesCollection.insertOne(package);
+            console.log(result);
+            res.json(result);
+        })
     }
     finally {
         // await client.close();
